d9eaf3779de5b7b2c00c2f1169489456af6ced44
dndn <denis.c1@live.fr>
2021-06-27 23:04:07 +0200

    test avec lancien gnl

--- get_next_line.c
+++ get_next_line.c
@@ -114,7 +114,7 @@ int	readbuff(char **buff, char **line)
 	len = endline - *buff;
 	*line = ft_substr(*buff, 0, len);
 	len++;
-	*buff = ft_substr(*buff, len, ft_strlen(*buff) - len)
+	*buff = ft_substr(*buff, len, ft_strlen(*buff) - len);
 	//*buff = ft_strdup(&((*buff)[len + 1]));
 	ret = 1;
     }
