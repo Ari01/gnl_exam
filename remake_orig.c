1b512201129f90e5989bd93e4c3546f6477079e8
dndn <denis.c1@live.fr>
2021-06-27 16:24:40 +0200

    test avec lancien gnl

---
+++ remake_orig.c
@@ -0,0 +1,147 @@
+#include "get_next_line.h"
+
+int	ft_strlen(char *s)
+{
+    int	    i;
+
+    i = 0;
+    while (s[i])
+	i++;
+    return (i);
+}
+
+char	*ft_strchr(char *s, char c)
+{
+    while (*s)
+    {
+	if (*s == c)
+	    return (s);
+	s++;
+    }
+    return (NULL);
+}
+
+char	*ft_strdup(char *s)
+{
+    char    *dup;
+    int	    i;
+
+    i = 0;
+    dup = malloc(sizeof(*dup) * (ft_strlen(s) + 1));
+    while (s[i])
+    {
+	dup[i] = s[i];
+	i++;
+    }
+    dup[i] = 0;
+    return (dup);
+}
+
+char	*ft_substr(char *s, int start, int len)
+{
+    char    *sub;
+    int	    i;
+
+    sub = malloc(sizeof(*sub) * (len + 1));
+    i = 0;
+    while (i < len)
+    {
+	sub[i] = s[start];
+	i++;
+	start++;
+    }
+    sub[i] = 0;
+    return (sub);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+    char    *join;
+    int	    i;
+
+    join = malloc(sizeof(*join) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+    i = 0;
+    while (*s1)
+    {
+	join[i] = *s1;
+	i++;
+	s1++;
+    }
+    while (*s2)
+    {
+	join[i] = *s2;
+	i++;
+	s2++;
+    }
+    join[i] = 0;
+    return (join);
+}
+
+int	readfile(char **buff)
+{
+    char    *freeptr;
+    char    tmp[100001];
+    int	    ret;
+
+    while ((ret = read(0, tmp, 100000)) > 0)
+    {
+	tmp[ret] = 0;
+	freeptr = *buff;
+	*buff = ft_strjoin(*buff, tmp);
+	free(freeptr);
+	if (ft_strchr(tmp, '\n'))
+	    return (1);
+    }
+    return (ret);
+}
+
+int	readbuff(char **buff, char **line)
+{
+    char    *endline;
+    char    *freeptr;
+    int	    len;
+
+    endline = ft_strchr(*buff, '\n');
+    freeptr = *buff;
+    len = endline - *buff;
+    *line = ft_substr(*buff, 0, len);
+    len++;
+    *buff = ft_substr(*buff, len, ft_strlen(*buff) - len);
+    free(freeptr);
+    return (1);
+}
+
+int	get_next_line(char **line)
+{
+    static char	*buff;
+    int		ret;
+
+    if (!buff)
+    {
+	buff = malloc(sizeof(*buff) * 2);
+	ret = read(0, buff, 1);
+	buff[ret] = 0;
+	if (*buff == '\n')
+	{
+	    free(buff);
+	    buff = 0;
+	    *line = ft_strdup("");
+	    return (1);
+	}
+    }
+    ret = readfile(&buff);
+    if (ret < 0)
+    {
+	free(buff);
+	buff = 0;
+	return (-1);
+    }
+    if (!ret && !*buff)
+    {
+	*line = ft_strdup("");
+	free(buff);
+	buff = 0;
+	return (0);
+    }
+    return (readbuff(&buff, line));
+}
